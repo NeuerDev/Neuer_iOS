6691c90a5fe19f7c9e2750c0616c1ed76cb425b4
lanyaSheng <lanyasheng1997@gmail.com>
2017-10-26 23:10:43 +0800

    修改回看model

--- NEUer/Source/Scene/Home/Model/Television/Channel/TelevisionChannelModel.h
+++ NEUer/Source/Scene/Home/Model/Television/Channel/TelevisionChannelModel.h
@@ -8,14 +8,53 @@
 
 #import <Foundation/Foundation.h>
 
+typedef NS_ENUM(NSInteger, TelevisionChannelModelSelectionType) {
+    TelevisionChannelModelSelectionTypeToday = 0,
+    TelevisionChannelModelSelectionTypeYesterday,
+    TelevisionChannelModelSelectionTypeTwoDaysAgo,
+    TelevisionChannelModelSelectionTypeThreeDaysAgo,
+    TelevisionChannelModelSelectionTypeFourDaysAgo,
+    TelevisionChannelModelSelectionTypeFiveDaysAgo,
+    TelevisionChannelModelSelectionTypeSixDaysAgo,
+    TelevisionChannelModelSelectionTypeSevenDaysAgo,
+};
+
+@protocol TelevisionChannelModelDelegate
+
+@required
+- (void)fetchTelevisionChannelModelSuccess;
+- (void)fetchTelevisionChannelModelFailureWithMsg:(NSString *)msg;
+
+@end
+
+
 @interface TelevisionChannelModel : NSObject
+
+@property (nonatomic, weak) id<TelevisionChannelModelDelegate> delegate;
+@property (nonatomic, assign) TelevisionChannelModelSelectionType selectedType;
+
+/**
+ @param videoUrl 播放源地址
+ */
+- (void)fecthTelevisionChannelDataWithVideoUrl:(NSString *)videoUrl;
+
+/**
+ 获取该播放源的近8天所有播放过的节目单
+ */
+- (NSDictionary *)TelevisionChannelDictionary;
+
+/**
+ 获取某一天播放过的节目单
+ */
+- (NSArray *)TelevisionChannelSelectionDayArrayWithType:(TelevisionChannelModelSelectionType)type;
 
 @end
 
 @interface TelevisionChannelScheduleBean : NSObject
 
-@property (nonatomic, strong) NSString *issue;
-@property (nonatomic, strong) NSString *status;
 @property (nonatomic, strong) NSString *videoUrl;
+@property (nonatomic, strong) NSString *date;
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) NSString *time;
 
 @end
--- NEUer/Source/Scene/Home/Model/Television/Channel/TelevisionChannelModel.m
+++ NEUer/Source/Scene/Home/Model/Television/Channel/TelevisionChannelModel.m
@@ -7,11 +7,148 @@
 //
 
 #import "TelevisionChannelModel.h"
+#import <hpple/TFHpple.h>
+#import "LYTool.h"
 
-@implementation TelevisionChannelScheduleBean
+@interface TelevisionChannelModel () <JHRequestDelegate>
+
+@property (nonatomic, strong) NSDictionary *TelevisionChannelDic;
+@property (nonatomic, strong) NSString *videoSource;
 
 @end
 
 @implementation TelevisionChannelModel
+
+#pragma mark - Init
+
+- (instancetype)init {
+    if (self = [super init]) {
+        _TelevisionChannelDic = [NSDictionary dictionary];
+    }
+    return self;
+}
+
+
+- (void)fecthTelevisionChannelDataWithVideoUrl:(NSString *)videoUrl {
+
+    _videoSource = [LYTool subStringFromString:videoUrl startString:@"hls/" endString:@".m3u8"];
+    NSString *urlStr = [NSString stringWithFormat:@"http://hdtv.neu6.edu.cn/newplayer?p=%@", self.videoSource];
+    JHRequest *request = [[JHRequest alloc] initWithUrl:[NSURL URLWithString:urlStr]];
+    request.delegate = self;
+    request.requestType = JHRequestTypeCancelPrevious;
+    [request start];
+}
+
+- (NSDictionary *)TelevisionChannelDictionary {
+    return self.TelevisionChannelDic;
+}
+
+- (NSArray *)TelevisionChannelSelectionDayArrayWithType:(TelevisionChannelModelSelectionType)type {
+    _selectedType = type;
+    if ([self.TelevisionChannelDic objectForKey:[LYTool dateOfTimeIntervalFromToday:self.selectedType]]) {
+        return [self.TelevisionChannelDic objectForKey:[LYTool dateOfTimeIntervalFromToday:self.selectedType]];
+    } else {
+        return nil;
+    }
+}
+
+#pragma mark - JHRequest
+
+- (void)requestDidFail:(JHRequest *)request {
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (_delegate) {
+            [_delegate fetchTelevisionChannelModelFailureWithMsg:@"加载失败！"];
+        }
+    });
+}
+
+- (void)requestDidSuccess:(JHRequest *)request {
+    
+    NSData *htmlData = request.response.data;
+    
+    [self updateTelevisionChannelModelDataWithHtmlData:htmlData];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (_delegate) {
+            [_delegate fetchTelevisionChannelModelSuccess];
+        }
+    });
+}
+
+#pragma mark - PrivateMethod
+
+- (void)updateTelevisionChannelModelDataWithHtmlData:(NSData *)htmlData {
+    TFHpple *hpple = [[TFHpple alloc ]initWithHTMLData:htmlData];
+    
+    NSMutableDictionary *listDic = [[NSMutableDictionary alloc] initWithCapacity:0];
+    NSMutableArray *listArr = [[NSMutableArray alloc] initWithCapacity:0];
+    
+    //    日期div
+    NSArray <TFHppleElement *>* dateArray = [hpple searchWithXPathQuery:@"//*[@id='tabs']/div"];
+    
+    for (TFHppleElement *dateElement in dateArray) {
+        
+        //        获取日期
+        NSString *date = dateElement.attributes[@"id"];
+        //        获取日期div的子div，即noon和afternoon
+        NSArray <TFHppleElement *>*tempElements = [dateElement childrenWithTagName:@"div"];
+        
+        //        先跳过noon和afternoon，直接取出一天的数据
+        for (TFHppleElement *elements in tempElements) {
+            
+            if ([elements.attributes[@"id"] isEqualToString:@"noon"] || [elements.attributes[@"id"] isEqualToString:@"afternoon"]) {
+                
+                TelevisionChannelScheduleBean *bean = [[TelevisionChannelScheduleBean alloc] init];
+                for (int i = 0; i < [elements childrenWithTagName:@"div"].count; ++i) {
+                    TFHppleElement *element = [elements childrenWithTagName:@"div"][i];
+                    bean.date = date;
+                    if ([element.attributes[@"id"] isEqualToString:@"list_status"]) {
+                        TFHppleElement *urlElement = [element firstChildWithTagName:@"a"];
+//                    转化成可以播放的视频地址
+//                        切换视频源
+                        NSArray *videoUrlArray = [[urlElement objectForKey:@"href"] componentsSeparatedByString:@"-"];
+                        NSMutableArray *videoMutableArray = [NSMutableArray arrayWithArray:videoUrlArray];
+                        [videoMutableArray removeLastObject];
+                        [videoMutableArray addObject:self.videoSource];
+                        NSString *videoRealUrl = [videoMutableArray componentsJoinedByString:@"-"];
+                        NSString *urlStr = [@"http://hdtv.neu6.edu.cn/" stringByAppendingString:videoRealUrl];
+                        
+                        bean.videoUrl = urlStr;
+                        
+                    } else if ([element.attributes[@"id"] isEqualToString:@"list_item"]) {
+                        NSString *text = element.text;
+                        NSArray *textArr = [text componentsSeparatedByString:@" "];
+                        bean.time = [textArr objectAtIndex:0];
+                        if (textArr.count > 2) {
+                            bean.name = [textArr componentsJoinedByString:@" "];
+                        } else {
+                            bean.name = [textArr lastObject];
+                        }
+                        
+                    } else {
+                        NSLog(@"%@", [element objectForKey:@"id"]);
+                    }
+                    if (i % 2 != 0) {
+                        if (bean.name && bean.time && bean.videoUrl) {
+                            [listArr addObject:bean];
+                            bean = [[TelevisionChannelScheduleBean alloc] init];
+                        }
+                    } else {
+                        continue;
+                    }
+                }
+            } else {
+                continue;
+            }
+        }
+        [listDic setObject:listArr.copy forKey:date];
+        [listArr removeAllObjects];
+    }
+    _TelevisionChannelDic = listDic;
+}
+
+@end
+
+@implementation TelevisionChannelScheduleBean
 
 @end
