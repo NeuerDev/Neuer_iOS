601ea05e8c7e18808989976d164030951a71a6ec
kl h <532128307@qq.com>
2017-11-05 14:40:20 +0800

    图书搜索界面修改

--- NEUer/Source/Scene/Campus/Controller/Library/Search/SearchLibraryViewController.m
+++ NEUer/Source/Scene/Campus/Controller/Library/Search/SearchLibraryViewController.m
@@ -8,19 +8,31 @@
 
 #import "SearchLibraryViewController.h"
 #import "SearchLibraryDoorViewController.h"
+#import "SearchLibraryBorrowingModel.h"
 
-#import "SearchListComponent.h"
+NSString * const kDefaultCellId = @"kDefaultCellId";
 
-@interface SearchLibraryViewController () <UISearchControllerDelegate, SearchListComponentDelegate>
+@interface SearchLibraryViewController () <UISearchControllerDelegate, UITableViewDelegate,UITableViewDataSource,SearchLibraryBorrowingDelegate>
 
 @property (nonatomic, strong) SearchLibraryDoorViewController *searchDoorViewController;
-
 @property (nonatomic, strong) UIScrollView *scrollView;
 @property (nonatomic, strong) UIView *contentView;
 @property (nonatomic, strong) UIVisualEffectView *maskView;
 @property (nonatomic, strong) UIBarButtonItem *collectionBarButtonItem;
-@property (nonatomic, strong) SearchListComponent *mostSearchComponent;
-@property (nonatomic, strong) SearchListComponent *recentSearchComponent;
+@property (nonatomic, strong) UITableView *tableView;
+//最近搜索
+@property (nonatomic, strong) UIView *recentView;
+@property (nonatomic, strong) UILabel *recentLabel;
+@property (nonatomic, strong) UIButton *recentBtn;
+//热门搜索
+@property (nonatomic, strong) UIView *mostView;
+@property (nonatomic, strong) UILabel *mostLabel;
+@property (nonatomic, strong) UIButton *mostBtn;
+@property (nonatomic, strong) SearchLibraryBorrowingModel *mostModel;
+
+@property (nonatomic, strong) NSArray<NSString *> *recentStrings;
+@property (nonatomic, strong) NSArray<NSString *> *mosetStrings;
+
 @end
 
 @implementation SearchLibraryViewController
@@ -42,8 +54,15 @@
         
     }
     
+    [self initData];
     [self initConstraints];
-    [self reloadData];
+}
+
+- (void)initData {
+    self.mostModel.languageType = @"ALL";
+    self.mostModel.sortType = @"ALL";
+    self.mostModel.date = @"y";
+    [self.mostModel search];
 }
 
 - (void)initConstraints {
@@ -62,40 +81,16 @@
         make.width.equalTo(self.scrollView);
     }];
     
-    [self.recentSearchComponent.view mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.and.right.and.top.equalTo(self.contentView);
+    [self.tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self.contentView);
+        make.height.mas_equalTo(12 * 44 + 100);
     }];
-    
-    [self.mostSearchComponent.view mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(self.recentSearchComponent.view.mas_bottom);
-        make.left.and.right.equalTo(self.contentView);
-        make.bottom.equalTo(self.contentView.mas_bottom);
-    }];
-}
 
-- (void)reloadData {
-    self.mostSearchComponent.strings = @[@"马克思原理", @"软件工程", @"公共事业管理基础", @"人工智能与神经网络", @"机械与自动化", @"工程原理"];
-    self.recentSearchComponent.strings = @[@"iOS", @"The Great Gatsby", @"Oliver Twist", @"The Phantom Of the Opera", @"Khaled Hosseini", @"莫言"];
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
 }
 
 #pragma mark - Response Methods
 
 - (void)showCollections {
-    
-}
-
-#pragma mark - SearchListComponentDelegate
-
-- (void)component:(SearchListComponent *)component didSelectedString:(NSString *)string {
-    self.searchDoorViewController.active = YES;
-    [self.searchDoorViewController searchKeyword:string];
-}
-
-- (void)component:(SearchListComponent *)component willPerformAction:(NSString *)action {
     
 }
 
@@ -133,6 +128,58 @@
     }
 }
 
+#pragma mark - UITableViewDelegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    if (section == 0) {
+        return self.recentView;
+    } else {
+        return self.mostView;
+    }
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 44.0f;
+}
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 2;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 6;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kDefaultCellId];
+    cell.textLabel.textColor = [UIColor beautyBlue];
+    cell.textLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleTitle3];
+    if (indexPath.section == 0) {
+        cell.textLabel.text = self.recentStrings[indexPath.row];
+    } else {
+        cell.textLabel.text = self.mosetStrings[indexPath.row];
+    }
+    return cell;
+}
+
+#pragma mark - SearchLibraryBorrowingDelegate
+- (void)searchDidSuccess {
+    NSMutableArray *array = [NSMutableArray array];
+    for (SearchLibraryBorrowingBean *bean in self.mostModel.resultArray) {
+        [array addObject:bean.title];
+    }
+    self.mosetStrings = array;
+    [self.tableView reloadData];
+}
+
+- (void)searchDidFail:(NSString *)message {
+    
+}
+
 #pragma mark - Getter
 
 - (SearchLibraryDoorViewController *)searchDoorViewController {
@@ -148,47 +195,24 @@
     if (!_collectionBarButtonItem) {
         _collectionBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"收藏" style:UIBarButtonItemStylePlain target:self action:@selector(showCollections)];
     }
-    
     return _collectionBarButtonItem;
 }
 
-- (SearchListComponent *)mostSearchComponent {
-    if (!_mostSearchComponent) {
-        _mostSearchComponent = [[SearchListComponent alloc] initWithTitle:NSLocalizedString(@"SearchLibrarySearchListMostSearchTitle", nil) action:@""];
-        _mostSearchComponent.delegate = self;
-        [self.contentView addSubview:_mostSearchComponent.view];
-    }
-    
-    return _mostSearchComponent;
-}
 
-- (SearchListComponent *)recentSearchComponent {
-    if (!_recentSearchComponent) {
-        _recentSearchComponent = [[SearchListComponent alloc] initWithTitle:NSLocalizedString(@"SearchLibrarySearchListRecentSearchTitle", nil) action:NSLocalizedString(@"SearchLibrarySearchListClear", nil)];
-        _recentSearchComponent.delegate = self;
-        [self.contentView addSubview:_recentSearchComponent.view];
-    }
-    
-    return _recentSearchComponent;
-}
 
 - (UIScrollView *)scrollView {
     if (!_scrollView) {
         _scrollView = [[UIScrollView alloc] init];
-        
         [self.view addSubview:_scrollView];
     }
-    
     return _scrollView;
 }
 
 - (UIView *)contentView {
     if (!_contentView) {
         _contentView = [[UIView alloc] init];
-        
         [self.scrollView addSubview:_contentView];
     }
-    
     return _contentView;
 }
 
@@ -198,8 +222,104 @@
         _maskView.hidden = YES;
         [self.view addSubview:_maskView];
     }
-    
     return _maskView;
+}
+
+- (UITableView *)tableView {
+   if (!_tableView) {
+       _tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
+       _tableView.dataSource = self;
+       _tableView.delegate =self;
+       _tableView.scrollEnabled = NO;
+       _tableView.showsVerticalScrollIndicator = NO;
+       _tableView.backgroundColor = [UIColor clearColor];
+       _tableView.separatorInset = UIEdgeInsetsMake(0, 16.0f, 0, 16.0f);
+       [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:kDefaultCellId];
+       [self.contentView addSubview:_tableView];
+    }
+   return _tableView;
+}
+
+- (UIView *)recentView {
+    if (!_recentView) {
+        _recentView = [[UIView alloc] init];
+        
+        _recentLabel = [[UILabel alloc] init];
+        _recentLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+        _recentLabel.text = @"最近搜索";
+        [_recentView addSubview:_recentLabel];
+        [_recentLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(_recentView).with.offset(16.0f);
+            make.left.equalTo(_recentView.mas_left).with.offset(16.0f);
+            make.height.mas_equalTo(34.0f);
+            make.bottom.equalTo(_recentView.mas_bottom);
+        }];
+        
+        _recentBtn = [[UIButton alloc] init];
+        [_recentBtn setTitleColor:[UIColor beautyBlue] forState:UIControlStateNormal];
+        [_recentBtn setTitle:@"清空" forState:UIControlStateNormal];
+        _recentBtn.titleLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleCallout];
+        [_recentView addSubview:_recentBtn];
+        [_recentBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(_recentView).with.offset(16.0f);
+            make.right.equalTo(_recentView.mas_right).with.offset(-16.0f);
+            make.height.mas_equalTo(34.0f);
+            make.bottom.equalTo(_recentView.mas_bottom);
+        }];
+    }
+   return _recentView;
+}
+
+- (UIView *)mostView {
+    if (!_mostView) {
+        _mostView = [[UIView alloc] init];
+        
+        _mostLabel = [[UILabel alloc] init];
+        _mostLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+        _mostLabel.text = @"热门搜索";
+        [_mostView addSubview:_mostLabel];
+        [_mostLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(_mostView).with.offset(16.0f);
+            make.left.equalTo(_mostView.mas_left).with.offset(16.0f);
+            make.height.mas_equalTo(34.0f);
+            make.bottom.equalTo(_mostView.mas_bottom);
+        }];
+        
+        _mostBtn = [[UIButton alloc] init];
+        [_mostBtn setTitleColor:[UIColor beautyBlue] forState:UIControlStateNormal];
+        [_mostBtn setTitle:@"清空" forState:UIControlStateNormal];
+        _mostBtn.titleLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleCallout];
+        [_mostView addSubview:_mostBtn];
+        [_mostBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(_mostView).with.offset(16.0f);
+            make.right.equalTo(_mostView.mas_right).with.offset(-16.0f);
+            make.height.mas_equalTo(34.0f);
+            make.bottom.equalTo(_mostView.mas_bottom);
+        }];
+    }
+   return _mostView;
+}
+
+- (NSArray<NSString *> *)recentStrings {
+    if (!_recentStrings) {
+        _recentStrings = @[@"iOS", @"The Great Gatsby", @"Oliver Twist", @"The Phantom Of the Opera", @"Khaled Hosseini", @"莫言"];
+    }
+   return _recentStrings;
+}
+
+- (NSArray<NSString *> *)mosetStrings {
+    if (!_mosetStrings) {
+        _mosetStrings = @[@"马克思原理", @"软件工程", @"公共事业管理基础", @"人工智能与神经网络", @"机械与自动化", @"工程原理"];
+    }
+   return _mosetStrings;
+}
+
+- (SearchLibraryBorrowingModel *)mostModel {
+    if (!_mostModel) {
+        _mostModel = [[SearchLibraryBorrowingModel alloc] init];
+        _mostModel.delegate = self;
+    }
+   return _mostModel;
 }
 
 @end
